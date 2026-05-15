b013a1472bc7a9ad5e9dd7e62367fcce0bafccab
KahlenHernani <Kahlenhernani@gmail.com>
2026-05-15 17:19:06 -0400

    Basic Frontend done for both Login and Sign up screens. Css style sheet made as well as Js files for index and sign up.

---
+++ js/login.js
@@ -0,0 +1,8 @@
+document
+  .getElementById("loginButton")
+  .addEventListener("click", login);
+
+function login()
+{
+  window.location.href = "contacts.html";
+}
---
+++ js/register.js
@@ -0,0 +1,8 @@
+document
+  .getElementById("registerButton")
+  .addEventListener("click", register);
+
+function register()
+{
+  window.location.href = "index.html";
+}
